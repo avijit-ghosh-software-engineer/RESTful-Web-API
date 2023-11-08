97086f75bd2e0c0e695e889e035c3afdbb1e6c84
Avijit Ghosh <avijitghsh11@gmail.com>
2023-11-09 00:24:03 +0530

    Done

--- src/MagicVilla_2/MagicVilla_VillaAPI/Program.cs
+++ src/MagicVilla_2/MagicVilla_VillaAPI/Program.cs
@@ -1,5 +1,8 @@
 using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Extensions;
+using MagicVilla_VillaAPI.Filters;
+using MagicVilla_VillaAPI.Middlewares;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,16 +19,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.BaseContainer();
 
-
-builder.Services.AddControllers(options=> {
-    //options.CacheProfiles.Add("Default30",
-    //   new CacheProfile()
-    //   {
-    //       Duration = 30
-    //   });
-    //options.ReturnHttpNotAcceptable = true;
-}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers(options => {
+	//options.CacheProfiles.Add("Default30",
+	//   new CacheProfile()
+	//   {
+	//	   Duration = 30
+	//   });
+	//options.ReturnHttpNotAcceptable = true;
+	options.Filters.Add<CustomExceptionFilter>();
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters().
+ConfigureApiBehaviorOptions(option =>
+{
+	option.ClientErrorMapping[StatusCodes.Status500InternalServerError] = new ClientErrorData
+	{
+		Link = "https://dotnetmastery.com/500"
+	};
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddResponseCaching();
@@ -53,11 +62,15 @@ else
 	});
 }
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.EnsureCreated();
-}
+//using (var scope = app.Services.CreateScope())
+//{
+//    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    dbContext.Database.EnsureCreated();
+//}
+
+//app.UseExceptionHandler("/ErrorHandling/ProcessError");
+//app.HandleError(app.Environment.IsDevelopment());
+app.UseMiddleware<CustomExceptionMiddleware>();
 
 app.UseStaticFiles();
 app.UseHttpsRedirection();
